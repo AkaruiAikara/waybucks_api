36fac599ecd41cc07ab80f7dd0ab9e2190d57026
--get <vall@aikara.me>
2022-02-10 07:26:40 +0700

    CRUD for User

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,2 +1,93 @@
 // import required models
 const { User } = require('../../models');
+
+// get users
+exports.getUsers = (req, res) => {
+    try {
+        User.findAll({
+            attributes: ['id', 'fullName', 'email', 'password']
+        }).then(users => {
+            res.send(users);
+        });
+    } catch (error) {
+        res.status(500).send({
+            message: error.message
+        });
+    }
+};
+
+// get user by id
+exports.getUserById = (req, res) => {
+    try {
+        User.findByPk(req.params.id, {
+            attributes: ['id', 'fullName', 'email', 'password']
+        }).then(user => {
+            if (!user) {
+                res.status(404).send({
+                    message: 'User not found'
+                });
+                return;
+            }
+            res.send(user);
+        });
+    } catch (error) {
+        res.status(500).send({
+            message: error.message
+        });
+    }
+};
+
+// add user
+exports.addUser = (req, res) => {
+    try {
+        User.create({
+            fullName: req.body.fullName,
+            email: req.body.email,
+            password: req.body.password
+        }).then(user => {
+            res.send(user);
+        });
+    } catch (error) {
+        res.status(500).send({
+            message: error.message
+        });
+    }
+};
+
+// update user
+exports.updateUser = (req, res) => {
+    try {
+        User.update({
+            fullName: req.body.fullName,
+            email: req.body.email,
+            password: req.body.password
+        }, {
+            where: {
+                id: req.params.id
+            }
+        }).then(user => {
+            res.send(user);
+        });
+    } catch (error) {
+        res.status(500).send({
+            message: error.message
+        });
+    }
+}
+
+// delete user
+exports.deleteUser = (req, res) => {
+    try {
+        User.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(user => {
+            res.send(user);
+        });
+    } catch (error) {
+        res.status(500).send({
+            message: error.message
+        });
+    }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,13 @@ const express = require('express')
 const router = express.Router()
 
 // controllers
+const { getUsers, getUserById, addUser, updateUser, deleteUser } = require('../controllers/user')
 
 // routes
+router.get('/users', getUsers)
+router.get('/users/:id', getUserById)
+router.post('/users', addUser)
+router.patch('/users/:id', updateUser)
+router.delete('/users/:id', deleteUser)
 
 module.exports = router
